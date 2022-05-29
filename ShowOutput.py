1802475d9b6db54d3ffde010cedbda5bd2637dac
Nitin Das <nitinhash1308@gmail.com>
2022-05-29 20:39:51 +0000

    :zap: Completed Widget integration

--- ShowOutput.py
+++ ShowOutput.py
@@ -8,19 +8,19 @@ from LSTMmodel import build_lstm_model
 def trainModel(lstm_neurons, dropout, loss, optimizer, batch_size, epochs, X_train, y_train, X_test, y_test):
 
     model = build_lstm_model(X_train, output_size=1, neurons=lstm_neurons, dropout=dropout, loss=loss,optimizer=optimizer)
-    history = model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=epochs, batch_size=batch_size, verbose=1, shuffle=True)
+    history = model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=epochs, batch_size=batch_size, verbose=2, shuffle=True)
 
     plt.plot(history.history['loss'],'r',linewidth=2, label='Train loss')
     plt.plot(history.history['val_loss'], 'g',linewidth=2, label='Validation loss')
     plt.title('LSTM')
     plt.xlabel('Epochs')
-    plt.ylabel('MSE')
+    plt.ylabel(loss.upper())
     plt.show()
 
     return model
 
         
-def predictModel(test, model,target_col,window_len,X_test):
+def predictModel(test, model,target_col,window_len,X_test,line_plot):
 
     targets = test[target_col][window_len:]
     preds = model.predict(X_test).squeeze()
--- finalRun.py
+++ finalRun.py
@@ -1,34 +1,36 @@
-'''
-from dataprepare import getRawData, prepare_data
-from showOutput import trainModel predictModel
-'''
+
+from dataprepare import getRawData, prepare_data, line_plot
+from ShowOutput import trainModel,predictModel
+
 def finalOutput(window_len = 5, test_size = 0.1, zero_base = True, lstm_neurons = 300, epochs = 50, batch_size = 36, loss = 'mse', dropout = 0.2, optimizer = 'adam'):
     
-   
     
-    optimzer = optimizer.lower()
     
-    mapper = {'Mean Squared Error (MSE)':'mse', 'Mean Absolute Error (MAE)':'mae', 'Huber Loss':'huberloss'}
+    print("Neurons -> ",lstm_neurons,"\nEpoch -> " ,epochs , "\nBatch Size -> ",batch_size , "\nLoss Function -> ",loss,"\nOptimizer-> ", optimizer )
+    
+    optimizer = optimizer.lower()
+    
+    mapper = {'Mean Squared Error (MSE)':'mse', 'Mean Absolute Error (MAE)':'mae', 'Huber Loss':'huberloss',"Binary Cross Entropy" : 'binary_crossentropy',"Hinge Loss" : 'hinge'}
     
     if loss in mapper:
         print(True)
         loss = mapper[loss]
         
-    print(lstm_neurons, epochs , batch_size , loss , optimizer )
+   # print("Neurons -> ",lstm_neurons,"Epoch -> " ,epochs , "Batch Size -> ",batch_size , "Loss Function -> ",loss,"Optimizer-> ", optimizer )
     
-'''
+
     target_col = "close"
     
     hist = getRawData()
 
     train, test, X_train, X_test, y_train, y_test = prepare_data(hist, target_col, window_len=window_len, zero_base=zero_base, test_size=test_size)
 
-    trainModel(lstm_neurons, dropout, loss, optimizer, batch_size, epochs, X_train, y_train, X_test, y_test)
+    model = trainModel(lstm_neurons, dropout, loss, optimizer, batch_size, epochs, X_train, y_train, X_test, y_test)
 
-    predictModel(test, model,target_col,window_len,X_test)
+    predictModel(test, model,target_col,window_len,X_test,line_plot)
 
     
-'''
+
     
 
     
